c59ef05ee273947530a6700482d502eb83c8bb6b
Carlos Silva <silvacj90@gmail.com>
2023-11-09 14:36:56 -0500

    Create full interactive and responsive webpage

--- index.js
+++ index.js
@@ -9,15 +9,15 @@ const state = {
 
 const getPlayersFromAPI = async () => {
   try {
-    const getPlayersEndpoint = await fetch(`${baseURL}players`);
-    const getPlayersJson = await getPlayersEndpoint.json();
-    const getPlayersObject = getPlayersJson.data;
+    const response = await fetch(`${baseURL}players`);
+    const jsonData = await response.json();
+    const playersObject = jsonData.data;
 
-    console.log("Success:", getPlayersObject);
-    updateState(getPlayersObject);
+    console.log("Success:", playersObject);
+    updateState(playersObject);
     render();
-  } catch (err) {
-    console.error("Error:", err);
+  } catch (error) {
+    console.error("Error:", error);
   }
 };
 
@@ -25,17 +25,48 @@ const updateState = (data) => {
   state.allPlayers = data.players;
 };
 
+const generateFormHTML = () => {
+  main.innerHTML = `
+  <article>
+    <section id="add-player">
+    <form>
+    <h2>Add Player</h2>
+      <label for="name">Name:</label>
+      <input name="name" id="name" type="text" />
+      
+      <label for="image">ImageURL</label>
+      <input name="image" id="image" type="text" />
+      
+      <label for="breed">Breed</label>
+      <input name="breed" id="breed" type="text" />
+
+      <input id="field" name="status" type="radio" value="field" />
+      <label for="field">Field</label>      
+
+      <input id="bench" name="status" type="radio" value="bench" />
+      <label for="bench">Bench</label>
+      <button>submit</button>
+    </form>
+    </section>                  
+  </article>
+  `;
+};
+
 const render = () => {
+  generateFormHTML();
+  getFormData();
   state.allPlayers.map((v) => {
-    console.log(v);
     generateCard(v);
   });
 };
 
 const generateCard = (data) => {
+  main.style.flexWrap = "wrap";
+
   const article = document.createElement("article");
   generateImg(article, data);
   generateHeading(article, data);
+  turnIntoLink(article, data);
   main.appendChild(article);
 };
 
@@ -45,13 +76,78 @@ const generateHeading = (article, data) => {
   article.appendChild(h2);
 };
 
-generateImg = (article, data) => {
+const generateImg = (article, data) => {
   const img = document.createElement("img");
   img.src = data.imageUrl;
   article.appendChild(img);
 };
 
+const turnIntoLink = (article, data) => {
+  article.addEventListener("click", () => {
+    main.style.flexWrap = "nowrap";
+    main.style.alignItems = "center";
+    const html = `
+      <div>
+      <img id="selected" src="${data.imageUrl}">
+      </div>
+      <div>
+      <h2>Name: ${data.name}</h2>
+      <p>Breed: ${data.breed}</p>
+      <p>Status: ${data.status}</p>
+      <button onClick="render()">Back to roster</button>
+      <button id="deletePlayer">Delete Player</button>
+      </div>
+    `;
+    main.innerHTML = html;
+    const deleteButton = document.querySelector("#deletePlayer");
+    deleteButton.addEventListener("click", () => {
+      deletePlayer(data);
+    });
+  });
+};
+
+const getFormData = () => {
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = form.querySelector('input[name="name"]').value;
+    const image = form.querySelector('input[name="image"]').value;
+    const breed = form.querySelector('input[name="breed"]').value;
+    const status = form.querySelector('input[name="status"]:checked').value;
+
+    const newPlayer = {
+      name: name,
+      imageUrl: image,
+      breed: breed,
+      status: status,
+    };
+    state.allPlayers.unshift(newPlayer);
+    pushPlayerDataToAPI(newPlayer);
+    render();
+  });
+};
+
+const pushPlayerDataToAPI = async (player) => {
+  try {
+    const response = await fetch(`${baseURL}players`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(player),
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+};
+
+const deletePlayer = (data) => {
+  const playerIndex = state.allPlayers.findIndex((v) => v.id === data.id);
+  state.allPlayers.splice(playerIndex, 1);
+  console.log(playerIndex);
+  render();
+};
+
 getPlayersFromAPI();
-// render the local state to the browser
 // allow for their to be a form for adding puppies
 // add option to remove puppies when you click one
